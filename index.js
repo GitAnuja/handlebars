4c3d00ce0be90e5d026fe5ff8abc1a4490170a79
Anuja Manoharan <anujamanoharan@gmail.com>
2017-09-09 11:04:42 +0530

    data

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var Handlebars = {
   compile : function(source){
-    return source;
+    return (function(data){
+      var index = source.indexOf("{{");
+      var ret = "";
+	  var close;
+      while(index > -1){
+		  ret+=source.substr(0, index-1);
+		  close = source.indexOf("}}");
+		  var property = source.substring(index+2, close);
+		  ret+=data[property];
+		  index = source.substr("{{", index);
+      }
+	  ret+=source.substr(close+2);
+      return ret;
+    });
   }
 }

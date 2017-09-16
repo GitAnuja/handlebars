1268de490a73fb105dede83e7847ecfa2de48c93
Anuja Manoharan <anujamanoharan@gmail.com>
2017-09-16 22:58:56 +0530

    block expressions

--- index.js
+++ index.js
@@ -15,12 +15,26 @@ var Handlebars = {
     return (function(data){
       var index = source.indexOf("{{");
       while(index > -1){
-		  var triple = false;
+		  var triple = false, helper, close = source.indexOf("}}");
 		  if(source[index+2] == "{"){
 			  triple = true;
 			  index++;
 		  }
-		  var close = source.indexOf("}}");
+		  else if(source[index+2] == "#"){
+			  var arg = source.substring(index+3, close).split(" ");
+			  helper = arg[0];
+			  arg = data[arg[1]];
+			  var end = "{{/"+helper+"}}";
+			  var close1 = source.indexOf(end, close+2);
+			  var temp = Handlebars.$.helper[helper](arg, {fn : function(v){
+				  var str = source.substring(close+2, close1);
+				  var str1 = Handlebars.replaceString(str, v);
+				  return str1;
+			  }});
+			  source = source.replace(source.substring(index, close1+end.length), temp);
+			  index = source.indexOf("{{");
+			  continue;
+		  }
 		  var property = source.substring(index+2, close);
 		  var val = data[property];
 		  if(triple){
@@ -42,5 +56,21 @@ var Handlebars = {
 	  "'" : '&#x27;',
 	  '`' : '&#x60;',
 	  '=': '&#x3D;'
+  },
+  registerHelper : function(name, func){
+	  this.$.helper[name] = func;
+  },
+  $ : {
+	  helper : {}
+  },
+  replaceString : function(str, val){
+	  var index = str.indexOf("{{");
+	  while(index > -1){
+		  var close = str.indexOf("}}");
+		  var property = str.substring(index+2, close);
+		  str = str.replace(new RegExp(str.substring(index, close+2), 'g'), val[property]);	  	
+		  index = str.indexOf("{{");
+	  }
+	  return str;
   }
 }
